6246f6cae8f8d79931d27d91f314a9b77d157532
Naifa Ardyani <80886018+naifardyani@users.noreply.github.com>
2024-12-30 11:45:35 +0700

    jQuery + perbaiki basic ulasan

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,36 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
+
+//Ulasan
+
+
+var multipleCardCarousel = document.querySelector(
+  "#carouselReview"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+
+  $(".carousel-control-next").on("click", function () {
+    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+      scrollPosition += cardWidth;  //update scroll position
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+    }
+  });
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $(".carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
