cd8e246e83b14f53578ee3b7b485897508f407e7
Naifa Ardyani <naifaza08@gmail.com>
2025-01-22 22:43:14 +0700

    double languange jadiii

--- javascript/script.js
+++ javascript/script.js
@@ -48,22 +48,3 @@ form.addEventListener('submit', function(event) {
 }); 
 
 //Double language
-const translations = {
-  en: {
-    "slogan": "Sunking in the Beauty of",
-    "desc-hero": "The beauty of Negeri kahyanganajgluhaeuighlshg.auerhghwebgawu"
-  },
-  id: {
-    "slogan": "Terbenam dalam Keindahan",
-    "desc-hero": "Nuansa negeri dongeng di lembah Gunung Merbabu bak negeri kahyangan yang asri dan pastinya instagramable. Dilengkapi dengan berbagai fasilitas yang lengkap serta nyaman membuat Negeri Kahyangan suatu destinasi wisata yang ramah keluarga."
-  }
-};
-
-function setLanguage(lang) {
-  $('[data-i18n]').each(function() {
-    const key = $(this).data('i18n');
-    $(this).text(translations[lang][key]);
-  });
-}
-
-setLanguage('en');
